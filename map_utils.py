cd17c12a0a9c09c63a9a8f662fa634730684c509
ChDo17 <chaitu.7.d@gmail.com>
2025-04-30 22:40:24 +0530

    Update map_utils.py

--- map_utils.py
+++ map_utils.py
@@ -2,7 +2,8 @@ import folium
 import streamlit.components.v1 as components
 
 def show_map():
-    m = folium.Map(location=[15.5, 73.8], zoom_start=10)
+    m = folium.Map(location=[15.5, 73.8],zoom_start=13,
+               tiles="CartoDB dark_matter"))
     folium.Marker([15.4952, 73.8261], popup="Sample RVM", icon=folium.Icon(color="blue")).add_to(m)
     folium.Marker([15.5200, 73.8400], popup="MRF Center", icon=folium.Icon(color="green")).add_to(m)
     components.html(m._repr_html_(), height=500)
